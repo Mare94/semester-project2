e5167db71e81f88a1c0aa146ca435ed8bbb76beb
Mare94 <mn-sand@online.no>
2023-09-14 19:28:26 +0200

    created auction listings

---
+++ src/js/api/auctions/get.mjs
@@ -0,0 +1,15 @@
+import { BASE_URL } from "../../constants/constants.mjs";
+
+export async function get() {
+    const url = `${BASE_URL}listings`;
+    const response = await fetch(url);
+
+    console.log(response);
+
+    if(response.ok) {
+        const json = await response.json();
+        return json;
+    }
+
+    throw new Error("There was an error loading the auctions");
+}
