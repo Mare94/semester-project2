67dcf1cc5c828bcee9d94b3ae510422c1d7e3ed5
Mare94 <mn-sand@online.no>
2023-09-19 18:12:50 +0200

    created createbid

---
+++ src/js/api/listings/createBid.mjs
@@ -0,0 +1,21 @@
+import { displayMessage } from "../../ui/common/index.mjs";
+import { authFetch } from "../authFetch.mjs";
+import { BASE_URL } from "../../constants/constants.mjs";
+
+const action = "/listings";
+
+export async function createListings(listingData) {
+
+    try {
+        const createListingsUrl = BASE_URL + action;
+        const response = await authFetch(createListingsUrl, {
+            method: "POST",
+            body: JSON.stringify(listingData)
+        })
+
+        window.location.reload()
+        return await response.json();
+    } catch(error) {
+        displayMessage("danger", error, "#message")
+    }
+}
