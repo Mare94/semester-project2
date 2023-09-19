2754dd04ac830560f9a6c1798d5b8c1698610d1c
Mare94 <mn-sand@online.no>
2023-09-19 17:57:39 +0200

    created bid

---
+++ src/js/api/listings/bid.mjs
@@ -0,0 +1,24 @@
+import { save, load } from "../../storage/storage.mjs";
+import { authFetch } from "../authFetch.mjs";
+import { BASE_URL } from "../../constants/constants.mjs";
+
+const action = "/listings";
+
+const queryString = document.location.search;
+const params = new URLSearchParams(queryString);
+const id = params.get("id");
+
+export async function listingBids(bidData) {
+
+    const profile = load("profile");
+    const listingBidsUrl = `${BASE_URL}${action}/${id}/bids`;
+
+    const response = await authFetch(listingBidsUrl, {
+        method: "POST",
+        body: JSON.stringify({amount: +bidData.amount})
+    })
+
+    window.location.reload();
+    save("profile", {...profile, credits: profile.credits - +bidData.amount})
+    return await response.json();
+}
