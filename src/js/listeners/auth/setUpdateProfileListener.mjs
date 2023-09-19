751dff8c471a3aa74c2e29c6a1efae5d3913baa1
Mare94 <mn-sand@online.no>
2023-09-19 16:46:43 +0200

    created update profile listener

---
+++ src/js/listeners/auth/setUpdateProfileListener.mjs
@@ -0,0 +1,37 @@
+
+import { load } from "../../storage/storage.mjs";
+
+// handles the updating of profiles
+
+export async function setUpdateProfileFormListener() {
+    const form = document.querySelector("#updateProfile");
+
+    if(form) {
+        const { name, email } = load("profile");
+
+        form.name.value = name;
+        form.email.value = email;
+
+        const button = form.querySelector("button");
+        button.disabled = true;
+
+        const profile = await readProfile(name);
+        
+        form.avatar.value = profile.avatar;
+
+        button.disabled = false;
+
+        form.addEventListener("submit", (event) =>{
+            event.preventDefault()
+            const form = event.target;
+
+            const formData = new formData(form);
+            const profile = Object.fromEntries(formData.enteries())
+
+            profile.name = name;
+            profile.email = email;
+
+            updateProfile(profile)
+        })
+    }
+};
