c36c119ef4737d8b6b49ec5a613b9d94f89c7e92
Mare94 <mn-sand@online.no>
2023-09-19 17:06:37 +0200

    created profile update

---
+++ src/js/api/profile/update.mjs
@@ -0,0 +1,19 @@
+import { authFetch } from "../authFetch.mjs";
+import { BASE_URL } from "../../constants/constants.mjs";
+
+const action = "/profile";
+const method = "put";
+
+export async function updateProfile(profileData) {
+    if(!profileData.name) {
+        throw new Error("Update requires an ID")
+    }
+
+    const updateProfileUrl = `${BASE_URL}${action}/${profileData.name}/media`;
+    const response = await authFetch(updateProfileUrl, {
+        method,
+        body: JSON.stringify(profileData)
+    });
+
+    return await response.json();
+}
