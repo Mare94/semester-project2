4167d1800791fb338424f79878761c41adcf0a9f
Mare94 <mn-sand@online.no>
2023-09-19 21:06:38 +0200

    created listing template

---
+++ src/js/templates/templatesListing.mjs
@@ -0,0 +1,35 @@
+import { displayMessage } from "../ui/common/displayMessage.mjs";
+
+export function templateListing(listingData) {
+    const readListing = document.createElement("div");
+    readListing.classList.add("listings")
+
+    try {
+        const endsAt = new Date(listingData.endsAt).toLocaleString();
+
+        const loader = document.querySelector("#loader");
+        loader.style.display = "none";
+
+        readListing.innerHTML = `
+                    <div class="card mb-3">
+                        <img src="${listingData?.media}" class="card-img-top" alt="${listingData.title}">
+                        <div class="card-body">
+                            <h5 class="card-title">${listingData.title}</h5>
+                            <p class="card-text">Bids: ${listingData._count.bids}</p>
+                            <p class="card-text"><a href="/listing/index.html?id=${listingData.id}"><button class="btn btn-secondary">Read more...</button></p>
+                        </div>
+                    </div>
+        `
+
+        return readListing;
+    } catch(error) {
+        displayMessage("type", error, "#message");
+    }
+}
+
+export function renderTemplateListing(listingDataList, parent) {
+    parent.innerHTML = ""
+    listingDataList.map(data =>{
+        parent.append(templateListing(data))
+    })
+};
