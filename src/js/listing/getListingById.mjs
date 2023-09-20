4f0748d62a964c7f08e75ac7a8e84444e199ce67
Mare94 <mn-sand@online.no>
2023-09-20 16:58:51 +0200

    updated listings by id

---
+++ src/js/listing/getListingById.mjs
@@ -0,0 +1,28 @@
+import * as templates from "../ui/templates/index.mjs";
+import * as listingsMethod from "../api/listings/index.mjs";
+
+
+// function to get listings by their ID
+// also looks at bid amount
+
+export async function getListingById() {
+    const listingContainer = document.querySelector("#listing");
+    const bidContainer = document.querySelector("#bid");
+
+    if(listingContainer) {
+        const listing = await listingsMethod.readListing();
+        const bidList = listing.bid.sort(function(a, b) {
+            const amountA = a.amount;
+            const amountB = b.amount;
+
+            if(amountA < amountB) return 1;
+            if(amountA > amountB) return -1;
+            return 0
+        });
+
+        templates.renderSingleListing(listing, listingContainer);
+        templates.renderBids(bidList, bidContainer);
+    }
+}
+
+getListingById();
