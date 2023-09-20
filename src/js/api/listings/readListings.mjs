66fee0bdf5c5c0f4001c2bc86ecd2428ead49ecb
Mare94 <mn-sand@online.no>
2023-09-20 17:35:26 +0200

    minor changes

--- src/js/api/listings/readListings.mjs
+++ src/js/api/listings/readListings.mjs
@@ -4,6 +4,9 @@ import { authFetch } from "../authFetch.mjs";
 const action = "/listings";
 const flag = "?_seller=true&_bids=true";
 
+
+// function to get all listings
+
 export async function readListings() {
     const getListingsUrl = `${BASE_URL}${action}`;
     const response = await authFetch(getListingsUrl);
@@ -11,10 +14,33 @@ export async function readListings() {
     return await response.json();
 }
 
+
+// function to search listings by id
+
+export async function searchListing(search) {
+
+    const id = params.get("id");
+
+    const searchListingsUrl = `${BASE_URL}${action}?sort=created&sortOrder=desc&${id}`;
+    const response = await authFetch(searchListingsUrl);
+    const result = await response.json();
+
+    const lowerCaseSearch = search.toLowerCase();
+    let filteredListing = result.filter(listing => listing.title.toLowerCase().includes(lowerCaseSearch))
+
+    return filteredListing;
+}
+
+
+
+// function to get listings by id
+
 export async function readListing() {
+
+    const id = params.get("id");
     const queryString = document.location.search;
     const params = new URLSearchParams(queryString);
-    const id = params.get("id");
+    
 
     const getListingUrl = `${BASE_URL}${action}/${id}${flag}`;
     const response = await authFetch(getListingUrl);
