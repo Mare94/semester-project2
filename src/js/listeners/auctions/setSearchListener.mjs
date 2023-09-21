87eb728fcae1e92c13f186c67279de1572c615af
Mare94 <mn-sand@online.no>
2023-09-21 18:32:23 +0200

    changed search listener

--- src/js/listeners/auctions/setSearchListener.mjs
+++ src/js/listeners/auctions/setSearchListener.mjs
@@ -1,6 +1,54 @@
 import { searchListing } from "../../api/listings/readListings.mjs";
 import { displayMessage } from "../../ui/common/displayMessage.mjs";
 
+
+
+export async function setSearchFormListener(){
+
+    const form = document.querySelector("#search");
+
+    if(form) {
+        form.addEventListener("submit", async (event) =>{
+
+            event.preventDefault();
+
+            const formData = new formData(event.target);
+
+            const SearchData = Object.fromEntries(formData.entries())
+
+            try{
+
+                await searchListing(SearchData.searchInput);
+                
+            } catch(error) {
+                displayMessage("danger", error, "#message");
+            }
+
+        });
+    }
+}
+
+setSearchFormListener();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 export async function setSearchFormListener() {
 
     const form = document.querySelector("#search")
@@ -22,4 +70,4 @@ export async function setSearchFormListener() {
     }
 }
 
-setSearchFormListener();
+setSearchFormListener(); */
