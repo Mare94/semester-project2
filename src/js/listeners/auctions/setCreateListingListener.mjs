5dee5fef74a61c2280de5df6b72e57c337967b2b
Mare94 <mn-sand@online.no>
2023-09-21 18:18:53 +0200

    changed function

--- src/js/listeners/auctions/setCreateListingListener.mjs
+++ src/js/listeners/auctions/setCreateListingListener.mjs
@@ -1,7 +1,54 @@
-import { createListings } from "../../api/listings/createBid.mjs";
+import { createListings } from "../../api/listings/index.mjs";
 import { displayMessage } from "../../ui/common/displayMessage.mjs";
 
 
+
+export async function setCreateListingListener() {
+
+    const form = document.querySelector("#createListing");
+
+    if(form) {
+
+        form.addEventListener("submit", async (event) => {
+                event.preventDefault()
+
+                const form = event.target
+                const formData = new formData(form);
+                const listing = Object.fromEntries(formData.entries())
+
+                try{
+
+                    await createListings(listing);
+                    displayMessage("success", "Listing has been created", "#message");
+                    
+                } catch(error){
+
+                    displayMessage("danger", error, "#message");
+                }
+                
+    })
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 export async function setCreateListingListener() {
 
     const form = document.querySelector("#createListing");
@@ -19,8 +66,11 @@ export async function setCreateListingListener() {
                 createListings({...listing})
             })
         }
+
+        displayMessage("success", "Listing created", "#message")
+
     } catch(error){
         displayMessage("type", error, "#message")
     }
 
-}
+} */
