90c7dcacbd3909e9db0fb4528db7327b9b9c66f7
Mare94 <mn-sand@online.no>
2023-09-21 18:25:54 +0200

    changed bid listener

--- src/js/listeners/auctions/setBidListener.mjs
+++ src/js/listeners/auctions/setBidListener.mjs
@@ -1,7 +1,54 @@
-import { listingBids } from "../../api/listings/bid.mjs";
+import { listingBids } from "../../api/listings/index.mjs";
 import { displayMessage } from "../../ui/common/index.mjs";
 
 
+
+export async function setBidFormListener(){
+
+    const form = document.querySelector("#createBid");
+
+    if(form) {
+        form.addEventListener("submit", async (event) =>{
+
+            event.preventDefault();
+
+            const form = event.target;
+            const formData = new formData(form);
+
+            const bidData = Object.fromEntries(formData.entries())
+
+            try{
+
+                await listingBids(bidData);
+                displayMessage("success", "A bid has been made", "#message");
+            } catch(error) {
+                displayMessage("danger", error, "#message");
+            }
+
+        });
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 export async function setBidFormListener() {
     
     const form = document.querySelector("#createBid");
@@ -23,3 +70,5 @@ export async function setBidFormListener() {
         displayMessage("danger", error, container);
     }
 }
+
+*/
