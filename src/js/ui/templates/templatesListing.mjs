d7b026c3a2028a4b32c293c526d18d61a7ada2fa
Mare94 <mn-sand@online.no>
2023-09-21 16:54:48 +0200

    added div

--- src/js/ui/templates/templatesListing.mjs
+++ src/js/ui/templates/templatesListing.mjs
@@ -11,11 +11,13 @@ export function templateListing(listingData) {
         loader.style.display = "none";
 
         readListing.innerHTML = `
+                    <div id="message"></div>
                     <div class="card mb-3">
                         <img src="${listingData.media}" class="card-img-top" alt="${listingData.title}">
                         <div class="card-body">
                             <h5 class="card-title">${listingData.title}</h5>
                             <p class="card-text">Bids: ${listingData._count.bids}</p>
+                            <p class="card-text">Deadline: ${endsAt}</p>
                             <p class="card-text"><a href="/listing/index.html?id=${listingData.id}"><button class="btn btn-secondary">Read more...</button></p>
                         </div>
                     </div>
