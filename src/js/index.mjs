0c6501812051d77bf9e784636339754268b642f0
Mare94 <mn-sand@online.no>
2023-09-22 18:29:51 +0200

    added listeners

---
+++ src/js/index.mjs
@@ -0,0 +1,36 @@
+import * as router from "../js/router/searchParams.mjs";
+import * as listeners from "../listeners/index.mjs";
+import * as listings from "../listing/index.mjs";
+import * as profiledata from "../api/profile/index.mjs";
+
+router.setSearchParams();
+
+const path = location.pathname;
+
+if (path === '/profile/login'){
+    listeners.setLoginFormListener();
+} else if( path === '/profile/register'){
+    listeners.setRegisterListener();
+} else if(path === '/profile/edit/'){
+    listeners.setUpdateProfileFormListener();
+} else if(path === ''){
+
+}
+
+
+
+listeners.setBidFormListener();
+
+listeners.setCreateListingListener();
+
+listeners.setLogoutFormListener();
+
+listeners.setSearchFormListener();
+
+
+
+listings.allListings();
+
+listings.listingById();
+
+profiledata.profileData();
