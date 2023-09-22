a908130cd4594683e87546d1a5c8693178d74da7
Mare94 <mn-sand@online.no>
2023-09-22 18:14:07 +0200

    added space

--- src/js/router/router.mjs
+++ src/js/router/router.mjs
@@ -3,14 +3,22 @@ import * as listings from "../listing/index.mjs";
 import * as profiledata from "../api/profile/index.mjs";
 
 listeners.setBidFormListener();
+
 listeners.setCreateListingListener();
+
 listeners.setLoginFormListener();
+
 listeners.setLogoutFormListener();
+
 listeners.setRegisterListener();
+
 listeners.setSearchFormListener();
+
 listeners.setUpdateProfileFormListener();
 
+
 listings.allListings();
+
 listings.listingById();
 
 profiledata.profileData();
