f9356a695638bd8f50d11d38e2729213f9bb5688
Mare94 <mn-sand@online.no>
2023-09-22 18:20:09 +0200

    updated router

--- src/js/router/router.mjs
+++ src/js/router/router.mjs
@@ -2,19 +2,30 @@ import * as listeners from "../listeners/index.mjs";
 import * as listings from "../listing/index.mjs";
 import * as profiledata from "../api/profile/index.mjs";
 
+
+
+const path = location.pathname;
+
+if (path === '/profile/login'){
+    listeners.setLoginFormListener();
+} else if( path === '/profile/register'){
+    listeners.setRegisterListener();
+} else if(path === '/profile/edit/'){
+    listeners.setUpdateProfileFormListener();
+} else if(path === ''){
+    
+}
+
+
+
 listeners.setBidFormListener();
 
 listeners.setCreateListingListener();
 
-listeners.setLoginFormListener();
-
 listeners.setLogoutFormListener();
-
-listeners.setRegisterListener();
 
 listeners.setSearchFormListener();
 
-listeners.setUpdateProfileFormListener();
 
 
 listings.allListings();
