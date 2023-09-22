8522da4d43d8ec16385da8eaa25ff07cf6d07142
Mare94 <mn-sand@online.no>
2023-09-22 19:20:26 +0200

    changed to correct api link

--- src/js/constants/constants.mjs
+++ src/js/constants/constants.mjs
@@ -1,1 +1,3 @@
-export const BASE_URL = "https://api.noroff.dev/v1/auction/";
+/*export const BASE_URL = "https://api.noroff.dev/v1/auction/";*/
+
+export const BASE_URL = "https://nf-api.onrender.com/api/v1/auction";
