f3ce38ab259c47e34b4fcc9c1600cd3b51710753
Mare94 <mn-sand@online.no>
2023-09-22 18:13:12 +0200

    created router

---
+++ src/js/router/router.mjs
@@ -0,0 +1,16 @@
+import * as listeners from "../listeners/index.mjs";
+import * as listings from "../listing/index.mjs";
+import * as profiledata from "../api/profile/index.mjs";
+
+listeners.setBidFormListener();
+listeners.setCreateListingListener();
+listeners.setLoginFormListener();
+listeners.setLogoutFormListener();
+listeners.setRegisterListener();
+listeners.setSearchFormListener();
+listeners.setUpdateProfileFormListener();
+
+listings.allListings();
+listings.listingById();
+
+profiledata.profileData();
