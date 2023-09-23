7f6b1dcb24f64c50fbf78b691eaf5decec088b8f
Mare94 <mn-sand@online.no>
2023-09-23 16:29:30 +0200

    added cors

--- src/js/constants/constants.mjs
+++ src/js/constants/constants.mjs
@@ -1,3 +1,9 @@
 /*export const BASE_URL = "https://api.noroff.dev/v1/auction/";*/
 
-export const BASE_URL = "https://nf-api.onrender.com/api/v1/auction";
+const corsEnabled = "https://noroffcors.onrender.com/";
+
+const url = "https://nf-api.onrender.com/api/v1/auction";
+
+
+export const BASE_URL = corsEnabled + url;
+
